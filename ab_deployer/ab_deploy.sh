0e2903790bc44f75e28a7ccdde7c3a9690d21341
RenatoEyllo <renato@eyllo.com>
2013-04-27 11:40:29 -0500

    Registering results

--- ab_deployer/ab_deploy.sh
+++ ab_deployer/ab_deploy.sh
@@ -2,6 +2,7 @@
 if [ -z "$1" ]
 then
 	echo "ERROR: OUTPUT FILE NAME REQUIRED."
+	echo "ab_deployer <outputFile> <configFile> <server>"
 	exit 1
 fi
 
@@ -19,13 +20,41 @@ then
 	exit 1
 fi
 
+if [ -z "$4" ]
+then
+	echo "ERROR: OPERATION REQUIRED."
+	exit 1
+fi
+
 # Defining variables needed
 CONF_FILE=$2
 OUTPUT_FILE=$1
 DEFAULT_EXEC_NUM=1
-LINES_TAKEN=30
+LINES_TAKEN=100
 SLEEP_TIME=5
 SERVER=$3
+COOR1=$[ ( $RANDOM % 180 )  + 1 ]
+COOR2=$[ ( $RANDOM % 180 )  + 1 ]
+
+# Defining operation requests
+READ_REQ="{action:1413,scenarioId:1,lat:-25,lng:-42,radius:1000}"
+
+WRITE_REQ="%7Baction:1310,accessToken=%22AAFFRRDDFFSFF%22,geotag:%7B%22title%22:%22teste%22,%22text%22:%22teste%22,%22infobox%22:%7B%22title%22:%22teste%22,%22text%22:%22asfdasfasdfsdafasdf%22%7D,%22location%22:%7B%22lng%22:${COOR1},%22lat%22:${COOR2}%7D,%22image%22:%7B%22id%22:%220%22,%22mime%22:%22%22%7D,%22type%22:%22text%22,%22uncodedTitle%22:%22teste%22,%22uncodedText%22:%22teste%22,%22fbUserId%22:123456789,%22fbChecked%22:false,%22userId%22:1,%22scenarioId%22:%221%22,%22id%22:%22%22,fbEmail:%22marta.silva@habanero.com%22,fbFstName=%22Marta%22,fbLstName=%22Silva%22,fbUserName=%22tt%22%7D%7D"
+
+if [ $4 == "read" ]
+then
+	REQUEST=${READ_REQ}
+fi
+if [ $4 == "write" ]
+then
+	REQUEST=${WRITE_REQ}
+fi
+
+LOADBALANCER=paprikaloadbalancer-1168239303.us-east-1.elb.amazonaws.com
+
+NUMBER=$[ ( $RANDOM % 100 )  + 1 ]
+
+#REQUEST=${WRITE_REQ}
 
 # Executing Apache AB command for a number of times
 echo "==============================="
@@ -43,7 +72,7 @@ do
 	NUM_THREADS=`echo $line | awk '{x=$2}END{print x}'`
 	echo "concurrentOps ${NUM_THREADS} totalReqs ${NUM_OPS}"
 
-	ab -A tomcat:tomcat -b 1024 -c ${NUM_THREADS}  -C 'scenarioId=1' -C 'radius=10000' -C 'll=-22.920734855121413,-43.2698221941406'  -n ${NUM_OPS} -r -v 4 http://${SERVER}:8080/paprika.json 2>&1 | tail -${LINES_TAKEN} >> ${OUTPUT_FILE}
+	ab -A tomcat:tomcat -b 1024 -c ${NUM_THREADS}  -n ${NUM_OPS} -r -v 4 http://${SERVER}:8080/servletMobile/mobile?param=${REQUEST} 2>&1 | tail -${LINES_TAKEN} >> ${OUTPUT_FILE}
 
 	# Separating outputs
 	echo "===============================================================================" >> $OUTPUT_FILE
